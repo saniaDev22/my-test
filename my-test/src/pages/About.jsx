725ba2a1b42183155eddeeb789498de9243e191e
saniadev22 <saniadev2222@gmail.com>
2026-02-17 11:16:21 +0530

    myyreop

--- my-test/src/pages/About.jsx
+++ my-test/src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from "../assets/imageone.jpeg"
 
 const About = () => {
   return (
@@ -25,7 +26,9 @@ const About = () => {
        <div className='border rounded-lg border-amber-50  md:flex items-center justify-center  '>
         <img className='h-50 w-50 md:h-[30%] md:w-[40%] object-cover rounded-2xl' src='https://thumbs.dreamstime.com/b/portrait-successful-business-people-working-together-corporate-office-portrait-successful-business-people-working-258098524.jpg '></img>
         
-        <div> <p className='p-10 text-[10px] md:text-[20px]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda illo tenetur eligendi praesentium enim velit laudantium ex delectus accusamus dolor Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum sequi aliquam officiis minus rem odit voluptatem minima itaque consequatur similique dolore et nesciunt cum animi, est temporibus ipsa in ratione..</p></div>
+       
+        <img className='h-50 w-50 md:h-[30%] md:w-[40%] object-cover rounded-2xl' src={Image}></img>
+     
       </div> 
       </div>
       </div>
